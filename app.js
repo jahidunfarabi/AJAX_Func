6bfddb60650c93d5220e1e8572a5edc527bd978f
jahidunfarabi <jahidunmuntaka25@gmail.com>
2026-01-10 00:04:14 +0600

    Modified js file

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 function loadProducts() {
-    var xhttp = new XMLHttpRequest();
+  var xhttp = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            // সার্ভার থেকে আসা JSON ডাটাকে অবজেক্টে রূপান্তর
-            var products = JSON.parse(this.responseText);
-            var output = "<ul>";
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var products = JSON.parse(this.responseText);
+      var output = "<ol>";
 
-            for (var i in products) {
-                output += "<li><strong>" + products[i].name + "</strong>: $" + products[i].price + "</li>";
-            }
+      for (var i in products) {
+        output +=
+          "<li><strong>" +
+          products[i].name +
+          "</strong>: $" +
+          products[i].price +
+          "</li>";
+      }
 
-            output += "</ul>";
-            document.getElementById("product-list").innerHTML = output;
-        }
-    };
+      output += "</ol>";
+      document.getElementById("product-list").innerHTML = output;
+    }
+  };
 
-    xhttp.open("GET", "products.json", true);
-    xhttp.send();
+  xhttp.open("GET", "products.json", true);
+  xhttp.send();
 }
